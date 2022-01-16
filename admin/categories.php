558cd4fd6b8f88082fe291e160b60b5c884eb66c
stab259 <nhientran2509@gmail.com>
2022-01-16 19:05:26 +0700

    Create the Admin Category Page and Add Categories

---
+++ admin/categories.php
@@ -0,0 +1,98 @@
+<?php include "includes/admin_header.php"; ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        WELCOME to admin
+                        <small>Author</small>
+                    </h1>
+
+                    <div class="col-xs-6">
+
+                        <?php
+                        if (isset($_POST['submit'])) {
+
+                            $cat_title = $_POST['cat_title'];
+
+                            if ($cat_title == "" || empty($cat_title)) {
+                                echo "This field should not be empty";
+                            } else {
+
+                                $query = "INSERT INTO categories(cat_title) ";
+                                $query .= "VALUE('{$cat_title}') ";
+
+                                $create_category_query = mysqli_query($connection, $query);
+
+                                header("Location: categories.php");
+                                // to prevent resending the previous request when we refresh the page after initially submitting the form
+
+                                if (!$create_category_query) {
+
+                                    die('QUERY FAILED' . mysqli_error($connection));
+                                }
+                            }
+                        }
+                        ?>
+
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat-title">Add Category</label>
+                                <input type="text" class="form-control" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" name="submit" value="Add Category">
+                            </div>
+                        </form>
+                    </div><!-- Add Category Form -->
+
+                    <div class="col-xs-6">
+
+                        <?php
+                        $query = "SELECT * FROM categories";
+                        $select_categories = mysqli_query($connection, $query);
+                        ?>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                while ($row = mysqli_fetch_assoc($select_categories)) {
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
+
+                                    echo "<tr>";
+                                    echo "<td>{$cat_id}</td>";
+                                    echo "<td>{$cat_title}</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
