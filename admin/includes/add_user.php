41c7832baa8668eab87bde8fc75a244ec3e62f93
stab259 <nhientran2509@gmail.com>
2022-01-23 18:31:17 +0700

    Improve CMS

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_POST['create_user'])) {
-    echo $user_firstname = $_POST['user_firstname'];
+    $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
 
@@ -21,6 +21,8 @@ if (isset($_POST['create_user'])) {
     $create_user_query = mysqli_query($connection, $query);
 
     confirmQuery($create_user_query);
+
+    echo "User Created: " . " " . "<a href='users.php'>View Users</a>";
 }
 
 ?>
