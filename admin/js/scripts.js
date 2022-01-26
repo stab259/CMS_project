8008558477c2bc0780f37d193581db199bb0fe64
stab259 <nhientran2509@gmail.com>
2022-01-26 11:46:49 +0700

    Add Bulk Options Posts

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
+
+  // summernote
   $('#summernote').summernote({
     height: 200
   });
+
+  // bulk_options js code in view_all_posts page
+  $('#selectAllBoxes').click(function (event) {
+    if (this.checked) {
+      $('.checkBoxes').each(function () {
+        this.checked = true;
+      });
+    } else {
+      $('.checkBoxes').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+
 });
