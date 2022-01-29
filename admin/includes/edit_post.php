e7c7a20ccd2c5adbd66c218b1c5cedea6a25a07b
stab259 <nhientran2509@gmail.com>
2022-01-29 16:03:43 +0700

    ADD - dropdown option, post creation notice (add_post) - link to view posts from admin(view_all_posts) - validation to comments (post) - dynamic personalization from admin

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -60,7 +60,7 @@ if (isset($_POST['update_post'])) {
 
     confirmQuery($update_post);
 
-    echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id=$post_id'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
+    echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id=$the_post_id'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
 }
 
 ?>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -40,7 +40,7 @@ if (isset($_POST['checkBoxArray'])) {
     </div>
     <div class="col-xs-4">
         <input type="submit" name="submit" class="btn btn-success" value="Apply">
-        <a href="add_post.php" class="btn btn-primary">Add New</a>
+        <a href="posts.php?source=add_post" class="btn btn-primary">Add New</a>
     </div>
     <table class="table table-bordered table-hover">
         <thead>
@@ -55,6 +55,7 @@ if (isset($_POST['checkBoxArray'])) {
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>View Post</th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
@@ -100,6 +101,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>{$post_tags}</td>";
                 echo "<td>{$post_comment_count}</td>";
                 echo "<td>{$post_date}</td>";
+                echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                 echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
                 echo "</tr>";
