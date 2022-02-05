243fa66c20f2ef80aff65c279c3f3eff66429be1
stab259 <nhientran2509@gmail.com>
2022-02-06 03:24:20 +0700

    Users ONLINE

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -25,3 +25,13 @@ $(document).ready(function () {
   });
 
 });
+
+function loadUsersOnline() {
+  $.get("functions.php?onlineusers=result", function (data) {
+    $('.usersonline').text(data);
+  });
+}
+
+setInterval(function () {
+  loadUsersOnline();
+}, 500);
